5166458756693b16d52d86199674c5cac9b91b08
Thea <pandelefl23@gmail.com>
2026-07-24 17:22:17 +0300

    Still working on spring migration.

--- SpringReddit/src/main/java/com/example/springreddit/model/PostVote.java
+++ SpringReddit/src/main/java/com/example/springreddit/model/PostVote.java
@@ -1,4 +1,60 @@
 package com.example.springreddit.model;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "post_votes")
 public class PostVote {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id", nullable = false)
+    private Post post;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "account_id", nullable = false)
+    private Account account;
+
+    @Column(name = "vote_direction", nullable = false)
+    private int voteDirection;
+
+    public PostVote() {
+    }
+
+    public PostVote(Post post, Account account, int voteDirection) {
+        this.post = post;
+        this.account = account;
+        this.voteDirection = voteDirection;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Post getPost() {
+        return post;
+    }
+
+    public Account getAccount() {
+        return account;
+    }
+
+    public int getVoteDirection() {
+        return voteDirection;
+    }
+
+    public void setVoteDirection(int voteDirection) {
+        this.voteDirection = voteDirection;
+    }
+
+    public void setPost(Post post) {
+        this.post = post;
+    }
+
+    public void setAccount(Account account) {
+        this.account = account;
+    }
 }
--- SpringReddit/src/main/java/com/example/springreddit/repository/PostVoteRepository.java
+++ SpringReddit/src/main/java/com/example/springreddit/repository/PostVoteRepository.java
@@ -1,4 +1,12 @@
 package com.example.springreddit.repository;
 
-public interface PostVoteRepository {
+import com.example.springreddit.model.*;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface PostVoteRepository extends JpaRepository<PostVote, Integer> {
+    Optional<PostVote> findByPostAndAccount(Post post, Account account);
 }
