bcc83efba26aa74867dafb632743c332dcaf839b
RARES AURELIAN RADUCANU <raducanurares40@gmail.com>
2026-08-03 19:24:06 +0300

    trimmed down subreddit code a bit

--- cli/App/App/src/bootstrap/SubredditModule.java
+++ cli/App/App/src/bootstrap/SubredditModule.java
@@ -7,7 +7,6 @@ import subreddit.SubredditMenu;
 import subreddit.command.CreateSubredditCommand;
 import subreddit.command.DeleteSubredditCommand;
 import subreddit.command.EditSubredditCommand;
-import subreddit.command.ViewSubredditCommand;
 import io.OutputWriter;
 import io.StringReader;
 
@@ -17,7 +16,7 @@ final class SubredditModule {
 
     static SubredditMenu create(SessionService sessionService, StringReader stringReader, OutputWriter output, PostClient postClient, PostView postView) {
         SubredditMenu subredditMenu = new SubredditMenu(sessionService, stringReader, output);
-        subredditMenu.registerCommand("1", new CreateSubredditCommand(sessionService));
+        subredditMenu.registerCommand("1", new CreateSubredditCommand(sessionService, stringReader, output));
         subredditMenu.registerCommand("2", new EditSubredditCommand(sessionService, stringReader));
         subredditMenu.registerCommand("3", new DeleteSubredditCommand(stringReader, sessionService));
         return subredditMenu;
--- cli/App/App/src/subreddit/command/CreateSubredditCommand.java
+++ cli/App/App/src/subreddit/command/CreateSubredditCommand.java
@@ -1,29 +1,41 @@
 package subreddit.command;
 
 import account.SessionService;
+import io.OutputWriter;
+import io.StringReader;
 import subreddit.Subreddit;
 import api.SubredditClient;
-import subreddit.model.SubDescription;
-import subreddit.model.SubName;
 
 import java.util.Objects;
 
 public class CreateSubredditCommand implements SubredditCommand {
     private final SessionService sessionService;
+    private final StringReader stringReader;
+    private final OutputWriter output;
 
-    public CreateSubredditCommand(SessionService sessionService) {
+    public CreateSubredditCommand(SessionService sessionService, StringReader stringReader, OutputWriter output) {
         this.sessionService = sessionService;
+        this.stringReader = stringReader;
+        this.output = output;
     }
 
     @Override
     public void execute(){
         System.out.println("Enter data or 0 to return to the previous menu:");
-        String subredditName = SubName.ask();
+        String subredditName = stringReader.readString("Subreddit name: ");
         if(Objects.equals(subredditName, "0")){
             return;
         }
-        String subredditDescription = SubDescription.ask();
+        if(subredditName.isBlank()){
+            output.write("Subreddit name cannot be empty!");
+            return;
+        }
+        String subredditDescription = stringReader.readString("Subreddit description: ");
         if(Objects.equals(subredditDescription, "0")){
+            return;
+        }
+        if(subredditDescription.isBlank()){
+            output.write("Subreddit description cannot be empty!");
             return;
         }
         Subreddit subreddit = new Subreddit(subredditName, subredditDescription, sessionService.getCurrentUsername());
