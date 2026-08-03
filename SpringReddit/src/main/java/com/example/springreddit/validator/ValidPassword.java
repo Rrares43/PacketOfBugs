28b59a15e2c83eeed906822cdec2b4d0e5e2819f
Thea <pandelefl23@gmail.com>
2026-08-03 13:26:19 +0300

    Fixed log in issue

--- SpringReddit/src/main/java/com/example/springreddit/validator/ValidPassword.java
+++ SpringReddit/src/main/java/com/example/springreddit/validator/ValidPassword.java
@@ -1,11 +1,18 @@
 package com.example.springreddit.validator;
 
 import jakarta.validation.Constraint;
-
+import jakarta.validation.Payload;
 import java.lang.annotation.*;
 
 @Documented
 @Constraint(validatedBy = PasswordConstraintValidator.class)
 @Target({ElementType.FIELD})
 @Retention(RetentionPolicy.RUNTIME)
-public @interface ValidPassword {}
+public @interface ValidPassword {
+
+    String message() default "Invalid password format";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+}
