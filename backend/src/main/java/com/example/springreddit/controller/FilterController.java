6cf541f7145b91aa895a761b479c54768455652d
sovarares <xrares2@gmail.com>
2026-08-09 12:01:23 +0300

    deleted unused code, refactored title

--- backend/src/main/java/com/example/springreddit/controller/FilterController.java
+++ backend/src/main/java/com/example/springreddit/controller/FilterController.java
@@ -3,8 +3,6 @@ package com.example.springreddit.controller;
 import com.example.springreddit.dto.FilterDto;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
