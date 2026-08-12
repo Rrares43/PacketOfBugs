064bc8cfcaf45f81bedf6d311be8c1522fc71b10
denysdobrynin07 <denisdobrynin07@gmail.com>
2026-08-12 20:12:42 +0300

    added javadoc comment on AuthenticationService

--- backend/src/main/java/com/example/springreddit/service/AuthenticationService.java
+++ backend/src/main/java/com/example/springreddit/service/AuthenticationService.java
@@ -10,11 +10,20 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
+/**
+ *  Handles operations with JWT authentication tokens
+ * @author Denys Dobrynin
+ */
 @Service
 @RequiredArgsConstructor
 public class AuthenticationService {
     private final AuthenticationManager authenticationManager;
 
+    /**
+     * Returns an Authentication object for a logged-in user
+     * @param loginRequest LoginRequest object with username and password fields
+     * @return Authentication object
+     */
     public Authentication getAuthentication(LoginRequest loginRequest) {
         return authenticationManager.authenticate(
                 new UsernamePasswordAuthenticationToken(
@@ -24,6 +33,10 @@ public class AuthenticationService {
         );
     }
 
+    /**
+     * Returns authenticated user's username from Security context or null if user is not logged in
+     * @return String with authenticated user's username
+     */
     public String currentUsernameOrNull() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         if (authentication == null
@@ -35,6 +48,11 @@ public class AuthenticationService {
         return authentication.getName();
     }
 
+    /**
+     * Returns authenticated user's username and throws an UnauthorizedException if username is null
+     * @return String with authenticated user's username
+     * @throws UnauthorizedException if the username is null
+     */
     public String requireAuthenticatedUsername() {
         String username = currentUsernameOrNull();
         if (username == null) {
