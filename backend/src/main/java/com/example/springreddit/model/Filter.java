d86620cae88317940cc9d9765b1b93ba2b1e7749
Stefan-Ciprian Gherghel <stefan.gherghel@stud.acs.upb.ro>
2026-08-13 22:23:46 +0300

    Add filter popularity & some logs.

--- backend/src/main/java/com/example/springreddit/model/Filter.java
+++ backend/src/main/java/com/example/springreddit/model/Filter.java
@@ -21,6 +21,9 @@ public class Filter {
     @Column(nullable = false, length = 50)
     private String label;
 
+    @Column(nullable = false, columnDefinition = "int default 0")
+    private Integer usageCount = 0;
+
     public Filter(Long id, String name, String label) {
         this.id = id;
         this.name = name;
--- backend/src/main/java/com/example/springreddit/service/FilterService.java
+++ backend/src/main/java/com/example/springreddit/service/FilterService.java
@@ -1,8 +1,12 @@
 package com.example.springreddit.service;
 
 import com.example.springreddit.dto.FilterDto;
+import com.example.springreddit.logging.CustomLogger;
+import com.example.springreddit.model.Filter;
 import com.example.springreddit.repository.FilterRepository;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -10,14 +14,42 @@ import java.util.stream.Collectors;
 
 @Service
 public class FilterService {
+
+    private static final CustomLogger LOGGER = CustomLogger.getInstance();
+
     private final FilterRepository filterRepository;
     public FilterService(FilterRepository filterRepository) {
         this.filterRepository = filterRepository;
     }
 
     public List<FilterDto> getAllFilters() {
-        return filterRepository.findAllByOrderByIdAsc().stream()
-                .map(filter -> new FilterDto(filter.getId(), filter.getName(), filter.getLabel()))
+
+        List<Filter> filters = filterRepository.findAllByOrderByIdAsc();
+
+        int maxUses = filters.stream()
+                .mapToInt(Filter::getUsageCount)
+                .max()
+                .orElse(0);
+
+        return filters.stream()
+                .map(filter -> {
+                    int count = filter.getUsageCount();
+                    String labelWithPopularity = filter.getLabel();
+
+                    if (count > 0 && count == maxUses) {
+                        labelWithPopularity += " 🔥";
+                    }
+
+                    return new FilterDto(filter.getId(), filter.getName(), labelWithPopularity);
+                })
                 .collect(Collectors.toList());
+    }
+
+    @Scheduled(cron = "0 0 0 * * ?")
+    @Transactional
+    public void resetFilterPopularity() {
+        LOGGER.info("Running scheduled job: Daily filter popularity reset...");
+        filterRepository.resetAllUsageCounts();
+        LOGGER.info("Filter popularity has been successfully reset to 0.");
     }
 }
