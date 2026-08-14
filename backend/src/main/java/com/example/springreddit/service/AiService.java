cbf91643d8431132b7b08c3236eb990c398a6268
RARES AURELIAN RADUCANU <raducanurares40@gmail.com>
2026-08-14 13:53:38 +0300

    another change

--- backend/src/main/java/com/example/springreddit/service/AiService.java
+++ backend/src/main/java/com/example/springreddit/service/AiService.java
@@ -31,7 +31,7 @@ public class AiService {
                     )
             );
 
-            String url = "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=" + apiKey;
+            String url = "https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=" + apiKey;
 
             JsonNode response = restClient.post()
                     .uri(url)
