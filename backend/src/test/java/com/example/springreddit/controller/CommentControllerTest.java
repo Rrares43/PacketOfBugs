6d341fb8c8697cf369a47ed09ee0658bd96d79d1
Pandele Florin <pandelefl23@gmail.com>
2026-08-17 18:04:23 +0300

    aaaaaaaaa

--- backend/src/test/java/com/example/springreddit/controller/CommentControllerTest.java
+++ backend/src/test/java/com/example/springreddit/controller/CommentControllerTest.java
@@ -1,27 +1,34 @@
 package com.example.springreddit.controller;
 
 import com.example.springreddit.config.JwtTokenProvider;
-import com.example.springreddit.dto.*;
-import com.example.springreddit.logging.CustomLogger;
+import com.example.springreddit.dto.CommentResponse;
+import com.example.springreddit.dto.CreateCommentRequest;
+import com.example.springreddit.dto.UpdateCommentRequest;
+import com.example.springreddit.dto.VoteRequest;
+import com.example.springreddit.dto.VoteResponse;
 import com.example.springreddit.service.CommentService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
-import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import tools.jackson.databind.ObjectMapper;
 
 import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -31,26 +38,24 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @WebMvcTest(CommentController.class)
 @AutoConfigureMockMvc(addFilters = false)
 public class CommentControllerTest {
+
     @Autowired
     private MockMvc mockMvc;
+
     @Autowired
     private ObjectMapper objectMapper;
-    @MockitoBean
-    private CustomLogger LOGGER = CustomLogger.getInstance();
-    @MockitoBean
+
+    @MockBean
     private CommentService commentService;
-    @MockitoBean
+
+    @MockBean
     private JwtTokenProvider jwtTokenProvider;
-    @MockitoBean
+
+    @MockBean
     private UserDetailsService userDetailsService;
 
     private static final String DELETE_MESSAGE = "The comment was deleted successfully";
 
-    /**
-     * Get comment by ID.
-     * Expected result: returns the mocked CommentResponse with no errors.
-     * @throws Exception
-     */
     @Test
     public void testGetComment() throws Exception {
         UUID postId = UUID.randomUUID();
@@ -67,7 +72,7 @@ public class CommentControllerTest {
                 "up",
                 Instant.now(),
                 Instant.now(),
-                new ArrayList<CommentResponse>()
+                new ArrayList<>()
         );
 
         when(commentService.getComment(response.id())).thenReturn(response);
@@ -78,17 +83,12 @@ public class CommentControllerTest {
                 .andExpect(jsonPath("$.data.content").value("test comment"));
     }
 
-    /**
-     * Create a valid comment.
-     * Expected result: returns the mocked CommentResponse with no errors.
-     * @throws Exception
-     */
     @Test
     public void testCreateComment() throws Exception {
         UUID postId = UUID.randomUUID();
         CreateCommentRequest request = new CreateCommentRequest(
-               "test_comment",
-               null
+                "test_comment",
+                null
         );
 
         CommentResponse response = new CommentResponse(
@@ -103,24 +103,19 @@ public class CommentControllerTest {
                 "up",
                 Instant.now(),
                 Instant.now(),
-                new ArrayList<CommentResponse>()
+                new ArrayList<>()
         );
 
         when(commentService.createComment(postId, request)).thenReturn(response);
 
-        mockMvc.perform(MockMvcRequestBuilders.post("/posts/{id}/comments", postId)
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(request)))
+        mockMvc.perform(post("/posts/{id}/comments", postId)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
                 .andExpect(status().isCreated())
                 .andExpect(jsonPath("$.success").value(true))
                 .andExpect(jsonPath("$.data.content").value("test comment"));
     }
 
-    /**
-     * Update an existing comment.
-     * Expected result: return the mocked CommentResponse with no errors.
-     * @throws Exception
-     */
     @Test
     public void testUpdateComment() throws Exception {
         UUID commentId = UUID.randomUUID();
@@ -138,12 +133,12 @@ public class CommentControllerTest {
                 "up",
                 Instant.now(),
                 Instant.now(),
-                new ArrayList<CommentResponse>()
+                new ArrayList<>()
         );
 
         when(commentService.updateComment(commentId, request)).thenReturn(response);
 
-        mockMvc.perform(MockMvcRequestBuilders.put("/comments/{id}", commentId)
+        mockMvc.perform(put("/comments/{id}", commentId)
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(request)))
                 .andExpect(status().isOk())
@@ -151,16 +146,11 @@ public class CommentControllerTest {
                 .andExpect(jsonPath("$.data.content").value("test comment"));
     }
 
-    /**
-     * Delete an existing comment.
-     * Expected result: commentService.deleteComment() invoked at least once.
-     * @throws Exception
-     */
     @Test
     public void testDeleteComment() throws Exception {
         UUID commentId = UUID.randomUUID();
 
-        mockMvc.perform(MockMvcRequestBuilders.delete("/comments/{id}", commentId))
+        mockMvc.perform(delete("/comments/{id}", commentId))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.success").value(true))
                 .andExpect(jsonPath("$.data.message").value(DELETE_MESSAGE));
@@ -168,26 +158,21 @@ public class CommentControllerTest {
         verify(commentService, times(1)).deleteComment(commentId);
     }
 
-    /**
-     * Leave a valid vote on a comment.
-     * Expected result: returns the mocked VoteResponse with no errors.
-     * @throws Exception
-     */
     @Test
     public void testVote() throws Exception {
         UUID commentId = UUID.randomUUID();
         VoteRequest request = new VoteRequest("up");
 
         VoteResponse response = new VoteResponse(
-                1,
-                0,
-                1,
+                1L,
+                0L,
+                1L,
                 "up"
         );
 
         when(commentService.vote(commentId, request)).thenReturn(response);
 
-        mockMvc.perform(MockMvcRequestBuilders.put("/comments/{id}/vote", commentId)
+        mockMvc.perform(put("/comments/{id}/vote", commentId)
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(request)))
                 .andExpect(status().isOk())
@@ -195,11 +180,6 @@ public class CommentControllerTest {
                 .andExpect(jsonPath("$.data.userVote").value("up"));
     }
 
-    /**
-     * Get comments by a valid post ID.
-     * Expected result: returns the mocked list of CommentResponse objects with no errors.
-     * @throws Exception
-     */
     @Test
     public void testGetCommentsByPostId() throws Exception {
         UUID postId = UUID.randomUUID();
@@ -216,7 +196,7 @@ public class CommentControllerTest {
                 "up",
                 Instant.now(),
                 Instant.now(),
-                new ArrayList<CommentResponse>()
+                new ArrayList<>()
         );
 
         List<CommentResponse> mockList = new ArrayList<>();
